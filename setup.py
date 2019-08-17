edec43c45c950ed54fcda88cbf9f41cfcfcdd3a3
Ram Seshadri <rseshadri@google.com>
2019-08-17 11:21:52 -0400

    Fixing Readme file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="autoviz",
-    version="0.0.2",
+    version="0.0.3",
     author="Ram Seshadri",
     # author_email="author@example.com",
     description="Automatically Visualize any dataset, any size with a single line of code",
