f255d13825644486b3c2d942855584807d9d006f
Ram Seshadri <rsesha2001@gmail.com>
2020-11-04 08:52:10 -0500

    Minor change to Plots directory.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="autoviz",
-    version="0.0.72",
+    version="0.0.73",
     author="Ram Seshadri",
     # author_email="author@example.com",
     description="Automatically Visualize any dataset, any size with a single line of code",
